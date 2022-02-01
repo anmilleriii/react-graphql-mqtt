0159ec8d243947d249addd522547b8d1efe25768
Al Miller <albert.miller@uconn.edu>
2022-01-31 17:02:15 -0800

    fix(config): fixing css in progress

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,14 +3,14 @@ import { Landing } from "./routes/Landing";
 import { About } from "./routes/About";
 import { NavigationBar } from "./components/Navigation/NavigationBar";
 
-import "./App.css";
+import "./App.module.css";
 
 export default () => {
   return (
     <div className="mv4 center w-third-l w-two-thirds-m w-two-thirds mid-gray vh-100 ">
       <BrowserRouter>
           <NavigationBar />
-          <h2>
+          <h2 className="test">
           MQTT Duplex Heartbeat over Broker PoC
           </h2>
         <Routes>
