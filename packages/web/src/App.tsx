aefeecee7eb93454f9e0f7e99ef310e8a9d9054d
Al Miller <albert.miller@uconn.edu>
2022-02-01 14:41:00 -0800

    refactor(general): general reorganize

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -5,17 +5,43 @@ import { Landing } from "./routes/Landing";
 import { About } from "./routes/About";
 import { NavigationBar } from "./components/Navigation/NavigationBar";
 
+/**
+ * Apollo
+ * @todo actual spot
+ */
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+
+/**
+ * GraphQL server endpoint
+ */
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 export default () => {
   return (
-    <div className="mv4 center w-third-l w-two-thirds-m w-two-thirds mid-gray vh-100 ">
-      <BrowserRouter>
-        <NavigationBar />
-        <h2 className="test">MQTT Duplex Heartbeat over Broker PoC</h2>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="mv4 center w-third-l w-two-thirds-m w-two-thirds mid-gray vh-100 ">
+        <BrowserRouter>
+          <NavigationBar />
+          <h2 className="test">MQTT Duplex Heartbeat over Broker PoC</h2>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
   );
 };
